86423dc87b1b9b3e4355175f85d55ece892aeb0c
Wilson Lessley <wilsonlessley14@gmail.com>
2024-11-24 23:17:10 -0500

    feat: texture on a square
    used an EBO to create a square
    
    applied texture as color in fragment shader
    
    textures

--- main.cpp
+++ main.cpp
@@ -17,31 +17,46 @@ int main(int argc, char** argv) {
   Shader shader = Shader(vertexShaderPath, fragmentShaderPath);
 
   float vertices[] = {
-    // positions          // colors
-    0.5f, -0.5f,  0.0f,   1.0f,  0.0f,  0.0f, // bottom right
-   -0.5f, -0.5f,  0.0f,   0.0f,  1.0f,  0.0f, // bottom left
-    0.0f,  0.5f,  0.0f,   0.0f,  0.0f,  1.0f, // top middle
+    // positions          // colors             // texture coords
+    0.5f,  0.5f,  0.0f,   1.0f,  0.0f,  0.0f,   1.0f,  1.0f,  // top right
+    0.5f, -0.5f,  0.0f,   0.0f,  1.0f,  0.0f,   1.0f,  0.0f,  // bottom right
+   -0.5f, -0.5f,  0.0f,   0.0f,  0.0f,  1.0f,   0.0f,  0.0f,  // bottom left
+   -0.5f,  0.5f,  0.0f,   1.0f,  1.0f,  0.0f,   0.0f,  1.0f,  // top left
   };
 
-  unsigned int VBO[2], VAO[2];
+  unsigned int indices[] = {
+    0, 1, 3,
+    1, 2, 3
+  };
+
+  unsigned int VBO[2], VAO[2], EBO;
   glGenVertexArrays(1, VAO);
   glGenBuffers(1, VBO);
+  glGenBuffers(1, &EBO);
 
   glBindVertexArray(VAO[0]);
   glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  // stride is 6 floats. first 3 are position values, next 3 are color values
-  // start at position 0, move 6 at a time
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  // stride is 8 floats. first 3 are position values, next 3 are color values, next 2 are texture coords
+  // start at position 0, move 8 at a time
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
 
-  // start at position 3, move 6 at a time
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+  // start at position 3, move 8 at a time
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
+
+  // start at position 6, move 8 at a time
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
 
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 
   //textures
@@ -64,7 +79,7 @@ int main(int argc, char** argv) {
   glGenerateMipmap(GL_TEXTURE_2D);
 
   stbi_image_free(data);
-
+  glBindTexture(GL_TEXTURE_2D, 0);
 
 
   while(!glfwWindowShouldClose(window)) {
@@ -73,10 +88,16 @@ int main(int argc, char** argv) {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    //uncomment next line for wireframe mode:
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     shader.use();
     shader.setFloat("offsetX", 0.5f);
     glBindVertexArray(VAO[0]);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glBindVertexArray(0);
     glfwPollEvents();
@@ -85,6 +106,7 @@ int main(int argc, char** argv) {
 
   glDeleteVertexArrays(1, VAO);
   glDeleteBuffers(1, VBO);
+  glDeleteBuffers(1, &EBO);
 
   glfwTerminate();
   return 0;
--- shaders/shader.fs
+++ shaders/shader.fs
@@ -1,7 +1,12 @@
 #version 330 core
+
 out vec4 FragColor;
+
 in vec3 vertexColor;
+in vec2 TextureCoord;
+
+uniform sampler2D ourTexture;
 
 void main() {
-  FragColor = vec4(vertexColor, 1.0);
+  FragColor = texture(ourTexture, TextureCoord) * vec4(vertexColor, 1.0);
 }
