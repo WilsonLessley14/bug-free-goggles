3fd9f683383e6c390e211865ce40403d543db70b
Wilson Lessley <wilsonlessley14@gmail.com>
2024-11-24 22:29:00 -0500

    feat: generating texture and storing in opengl texture object
    textures

--- main.cpp
+++ main.cpp
@@ -45,15 +45,27 @@ int main(int argc, char** argv) {
 
 
   //textures
-  int width, height, channelsCount;
-  unsigned char *data = stbi_load("textures/container.jpg", &width, &height, &channelsCount, 0);
-
   unsigned int texture;
   glGenTextures(1, &texture);
-
   glBindTexture(GL_TEXTURE_2D, texture);
 
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  int width, height, channelsCount;
+  unsigned char *data = stbi_load("textures/container.jpg", &width, &height, &channelsCount, 0);
+  if (!data) {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+  glGenerateMipmap(GL_TEXTURE_2D);
+
+  stbi_image_free(data);
+
+
 
   while(!glfwWindowShouldClose(window)) {
     manager.processInput();
