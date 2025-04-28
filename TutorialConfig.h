d27d91fc214e912a163ded3305a02a334fdfe0ce
WilsonLessley14 <wilsonlessley14@gmail.com>
2025-04-28 14:46:01 -0700

    feat(cmake): version number as an env variable

---
+++ TutorialConfig.h
@@ -0,0 +1,5 @@
+// the configured options and settings for Tutorial
+// TODO 10: Define Tutorial_VERSION_MAJOR and Tutorial_VERSION_MINOR
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+
--- tutorial.cxx
+++ tutorial.cxx
@@ -2,14 +2,15 @@
 #include <cmath>
 #include <iostream>
 #include <string>
+#include <TutorialConfig.h>
 
 // TODO 11: Include TutorialConfig.h
 
 int main(int argc, char* argv[])
 {
   if (argc < 2) {
-    // TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
-    //          and Tutorial_VERSION_MINOR
+    std::cout << argv[0] << " Version " << VERSION_MAJOR << "."
+              << VERSION_MINOR << std::endl;
     std::cout << "Usage: " << argv[0] << " number" << std::endl;
     return 1;
   }
